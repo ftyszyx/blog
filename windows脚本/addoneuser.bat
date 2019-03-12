75da93103a088196b46256a8e80fd2e0f5be4fd3
zhangyuxin <whyzi@qq.com>
2019-03-12 23:32:43 +0800

    zyx

---
+++ windows脚本/addoneuser.bat
@@ -0,0 +1,23 @@
+﻿@echo off
+
+set username=
+set nickname=
+set password=
+
+set /p username=input username:
+set /p password=input password:
+
+set folderpath=D:\%username%
+NET USER %username% %password% /add /comment:"Account for User" /expires:never  
+net localgroup "Remote Desktop Users" %username% /add
+
+if exist %folderpath% (
+	echo "folder exit"
+	) else (
+		md %folderpath%
+		echo y|cacls.exe %folderpath% /c /e /t /r users
+		echo y|cacls.exe %folderpath% /c /e /t /p %username%:f
+	)
+
+
+NET USER %username%  /HOMEDIR:%folderpath%
---
+++ windows脚本/addusercsv.bat
@@ -0,0 +1,24 @@
+﻿@echo off
+
+
+for /f "tokens=1-3 delims=," %%a in  ('type "user.csv"') do (
+echo "username"%%a 
+echo "nickname"%%b 
+echo "password"%%c 
+echo "folderpath"D:\%%b
+
+NET USER %%a %%c /add /comment:"Account for User" /expires:never  
+net localgroup "Remote Desktop Users" %%a /add
+
+if exist D:\%%b (
+echo "folder exit"
+) else (
+	md D:\%%b
+	echo y|cacls.exe D:\%%b  /c /e /t /r users
+	echo y|cacls.exe D:\%%b  /c /e /t /p %%a:f
+)
+
+NET USER %%a  /HOMEDIR:D:\%%b
+)
+
+
