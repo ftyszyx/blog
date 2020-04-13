91610aed43ee0d3d109ee9bc4d2a89ccbeaaa597
zyx <whyzi@qq.com>
2020-04-13 18:21:02 +0800

    zyx

--- ml/hw1/train.py
+++ ml/hw1/train.py
@@ -1,39 +1,56 @@
 import sys
 import pandas as pd
 import numpy as np
+import  math
 data=pd.read_csv("./train.csv",encoding="big5")
+data=data.iloc[:,3:]# 取数据
+data[data=="NR"]=0
+raw_data=data.values#转数组 18
 
-data=data.iloc[:,3:]
-data[data=='NR']=0
-raw_data=data.values
+#将数据
+#以月为单位
+#12个月  每个月20天  一天24小时 ，每小时18个数据
+#
+#每9小时生成一份data,
+#每9小时有18*9个特征，然后这些特征对应一个p2.5的值
+#每个月对应471个特征组
 
+#以月为单位操作，所以把数据按月排列先
 month_data={}
-#一个月20天*24小时，有18个数据项
-#相当于一个月480小时
 for month in range(12):
     sample=np.empty([18,480])
     for day in range(20):
-        sample[:,day*24:(day+1)*24]=raw_data[18 * (20 * month + day) : 18 * (20 * month + day + 1), :]
+        sample[:,day*24:(day+1)*24]=raw_data[18*(20*month+day):18*(20*month+day+1),:]
     month_data[month]=sample
-#每個月會有 480hrs，每 9 小時形成一個 data，每個月會有 471 個 data，故總資料數為 471 * 12 筆，而每筆 data 有 9 * 18 的 features (一小時 18 個 features * 9 小時)。
-#對應的 target 則有 471 * 12 個(第 10 個小時的 PM2.5)
-x = np.empty([12 * 471, 18 * 9], dtype = float)
-y = np.empty([12 * 471, 1], dtype = float)
+
+#生成x和y
+x=np.empty([12*471,18*9],dtype=float)
+y=np.empty([12*471,1],dtype=float)
 for month in range(12):
     for day in range(20):
         for hour in range(24):
-            if day == 19 and hour > 14:
+            if day==19 and hour>14: #day和hour都是从0开始
                 continue
-            x[month * 471 + day * 24 + hour, :] = month_data[month][:,day * 24 + hour : day * 24 + hour + 9].reshape(1, -1) #vector dim:18*9 (9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9)
-            y[month * 471 + day * 24 + hour, 0] = month_data[month][9, day * 24 + hour + 9] #value
-print(x)
-print(y)
+            row = month*471+day*24+hour
+            #第n行是把当前小时+9小时的数据取出来，按照特征1*小时1 特征1*小时2
+            x[row,:]=month_data[month][:,day*24+hour:day*24+hour+9].reshape(1,-1)#
+            y[row,0]=month_data[month][9,day*24+hour+9]#值
+
+#print(x)
+#对x的特征做归一化，u=0
+meanx=np.mean(x,axis=0)
+std_x=np.std(x,axis=0)
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        if std_x[j]!=0:
+            x[i][j]=(x[i][j]-meanx[j])/std_x[j]
 
 
-mean_x = np.mean(x, axis = 0) #18 * 9
-std_x = np.std(x, axis = 0) #18 * 9
-for i in range(len(x)): #12 * 471
-    for j in range(len(x[0])): #18 * 9
-        if std_x[j] != 0:
-            x[i][j] = (x[i][j] - mean_x[j]) / std_x[j]
-x
+#将数据分成训练和测试数据
+x_train_set=x[:math.floor(len(x)*0.8),:]
+y_train_set=y[:math.floor(len(y)*0.8),:]
+x_validation=x[math.floor(len(x)*0.8):,:]
+y_validation=y[math.floor(len(y)*0.8):,:]
+
+#开始训练
+#y=wx+b==>
